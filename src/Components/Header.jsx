89d007e11dd5d1b9a5574c16d1f2b5e0aa0e22cd
Bilal Hamza <Bilalhamzacaptain.work@gmail.com>
2023-06-11 04:04:04 +0500

    new file added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -25,6 +25,7 @@ const Header = () => {
       <a href="#about">About</a>
       <a href="#portfolio">Portfolio</a>
       <a href="#footer">Contact</a>
+      <a href="maryam.html">Maryam</a>
     </div>
   );
 };
